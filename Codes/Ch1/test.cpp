6f693b252763c34e9d76e757804debbe93f4a6bd
1255108389 <1255108389@qq.com>
2022-08-31 23:34:23 +0800

    add test

---
+++ Codes/Ch1/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 12551 on 2022/8/31.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Test\n";
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/CppPrimerCodes.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppPrimerCodes.dir/Codes/Ch1/test.cpp.obj"
+  "CMakeFiles/CppPrimerCodes.dir/Codes/Ch1/test.cpp.obj.d"
+  "CppPrimerCodes.exe"
+  "CppPrimerCodes.exe.manifest"
+  "CppPrimerCodes.pdb"
+  "libCppPrimerCodes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppPrimerCodes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
